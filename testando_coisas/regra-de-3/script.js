b060f2a9a8c14119d5b393c01d5f57ee091dffe7
gustavomatiass <gustavomatiass123@gmail.com>
2022-06-17 16:07:57 -0300

    testando

--- testando_coisas/regra-de-3/script.js
+++ testando_coisas/regra-de-3/script.js
@@ -2,9 +2,18 @@ function clicar() {
     var num1 = document.querySelector("#n1").value;
     var num2 = document.querySelector("#n2").value;
 
-    var resultado = parseInt(num1) + parseInt(num2);
+    var num3 = document.querySelector("#n3").value;
+
+    //soma
+    var multiplica = parseInt(num1) * parseInt(num2);
+
+    
+    var resultado = multiplica / num3;
     
     document.querySelector(".resultado").innerHTML = resultado
 
     console.log(resultado)
+    console.log(num3)
 }
+
+ document.getElementById("cor").addEventListener("click", "green" )
