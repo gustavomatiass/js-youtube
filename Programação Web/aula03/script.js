3ddcf4de5921250a68aa2a5ccb1b1708b1ba633a
gustavomatiass <gustavomatiass123@gmail.com>
2023-01-16 10:04:55 -0300

    eventos

--- Programação Web/aula03/script.js
+++ Programação Web/aula03/script.js
@@ -9,3 +9,29 @@ function eventoDblClick(){
     alert('evento de click dublo');
 }
 
+function viraAmarelo(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "green";
+}
+
+function viraAzul(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "blue";
+}
+
+function dicionaTexo(){
+    let p = document.getElementById("texto");
+    p.append('o mause moveu<br>');
+}
+
+function limpaTexto(){
+    document.getElementById("compoTexto").value = "";
+}
+
+function mudou(){
+    console.log('mudou');
+}
+
+function teclaPressionada(){
+    console.log("tecla pressionada");
+}
