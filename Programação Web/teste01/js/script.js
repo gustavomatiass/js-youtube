915eef48c21e593d5af1b65f0e7a72a1c66b59ab
gustavomatiass <gustavomatiass123@gmail.com>
2023-01-17 10:18:47 -0300

    projeto calc

--- Programação Web/teste01/js/script.js
+++ Programação Web/teste01/js/script.js
@@ -1,4 +1,67 @@
-function convert(){
-    document.getElementById("real");
-    let real =  
+
+document.addEventListener("DOMContentLoaded", function(){
+    let real = document.getElementById("real");
+    let resultado = document.getElementById("result");
+    const dolar = 5.14;
+    document.getElementById("send").addEventListener("click", function(){
+      if(real.value){
+      resultado.innerHTML = dolar * parseFloat(real.value).toFixed(2);
+      }
+    });
+  });
+
+/*
+// pegando valores
+
+let real = parseFloat(document.getElementById("real").value);
+
+let resultado = document.getElementById("result").innerHTML;
+
+const dola = 5.14;
+
+function calculate(){
+
+    resultado = dola * real;
+
+    document.getElementById("result").innerHTML = resultado;
 }
+
+document.getElementById("send").addEventListener("click", function(){
+    calculate();
+});
+
+
+
+
+function re(){
+
+    dola = 5.14;
+    resultado = dola * real;
+
+    const btn = document.getElementById("send");
+
+    document.getElementById("send").addEventListener("click", function(){
+        calculte(real);
+    });
+
+    document.getElementById("result").innerHTML = resultado;
+
+};
+
+
+
+const btn = document.getElementById("send");
+
+btn.addEventListener("click", function(e){
+
+        e.preventDefault();
+    const real = document.getElementById("real");
+    const dola = document.getElementById("dolar");
+    var x;
+
+    x = dola * real;
+
+
+
+});
+*/
