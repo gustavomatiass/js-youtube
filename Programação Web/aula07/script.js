5ce3077e565ca2a20adbae6f9a33afe3fd71654c
gustavomatiass <gustavomatiass123@gmail.com>
2023-01-19 10:12:55 -0300

    operadores

---
+++ Programação Web/aula07/script.js
@@ -0,0 +1,40 @@
+// NOT-!
+
+    if(true){
+        console.log("passou");
+    }
+
+    var nome = "matheus";
+
+    if(!(nome == "matheu")){
+        console.log("ok");
+    }
+
+//OR-||
+/*
+    var idade = 11;
+    var nome = "joão";
+
+    if(nome == "joão" || idade >= 14){
+        console.log("pode participar da aula de esgrima")
+    } else {
+        console.log("não pode entrar");
+    }
+
+    if(nome == "joão" || (15 > 20 && 10==10)){
+        console.log("testando")
+    } else {
+        console.log("erro")
+    }
+
+// &&
+
+
+var idade = 13;
+var nome = "joão";
+
+if(nome == "joão" && idade == 16){
+    console.log("poder participar da aula de esgrima");
+} else {
+    console.log("vc não pode participar, faça a seua inscrição");
+}*/
