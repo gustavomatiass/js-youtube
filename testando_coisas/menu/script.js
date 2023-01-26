1e95e905aa3e2afb50135d8250d6b35186639e93
gustavomatiass <gustavomatiass123@gmail.com>
2023-01-26 10:45:10 -0300

    menu

--- testando_coisas/menu/script.js
+++ testando_coisas/menu/script.js
@@ -10,12 +10,39 @@ var n = document.getElementById('nca');
 n.addEventListener('mousemove', ncs)
 n.addEventListener('mouseout', nfora)
 
+var skills = document.getElementById('sk');
+skills.addEventListener('mousemove', sk)
+skills.addEventListener('mouseout', skfora)
+
+var extras = document.getElementById('ex')
+extras.addEventListener('mousemove', ex)
+extras.addEventListener('mouseout', exfora)
+
+var contato = document.getElementById('co')
+contato.addEventListener('mousemove', co)
+contato.addEventListener('mouseout', cofora)
 
 function home(){
     h.style.color = 'black'
     h.style.fontSize = '30pt'
     var corpo = document.body
     corpo.classList.add('degrade')
+    var nome = document.getElementById('nomes');
+    nome.innerHTML = 'home'
+
+    /*
+    var h1 = document.createElement("h1");
+    var text = document.createTextNode("home");
+    h1.appendChild(text);
+    h1.style.textAlign = "center";
+    h1.style.position = "absolute";
+    h1.style.zIndex = "-1";
+    h1.style.margin = '-150px'
+    h1.style.height = '40px'
+    h1.style.fontSize = '200pt'
+    h1.style.opacity = '0.1'
+    h.appendChild(h1)
+    */
 }
 
 function fora(){
@@ -23,6 +50,10 @@ function fora(){
     h.style.fontSize = '20pt'
     var corpo = document.body
     corpo.classList.remove('degrade')
+    var nome = document.getElementById('nomes')
+    nome.innerHTML = ''
+    
+    
 }
 
 function blogs(){
@@ -30,6 +61,8 @@ function blogs(){
     b.style.fontSize = '30pt'
     var corpo = document.body
     corpo.classList.add('degrade2')
+    var nome = document.getElementById('nomes');
+    nome.innerHTML = 'blogs'
 }
 
 function bfora(){
@@ -37,6 +70,8 @@ function bfora(){
     b.style.fontSize = '20pt'
     var corpo = document.body
     corpo.classList.remove('degrade2')
+    var nome = document.getElementById('nomes')
+    nome.innerHTML = ''
 }
 
 function ncs(){
@@ -44,6 +79,10 @@ function ncs(){
     n.style.fontSize = '30pt'
     var corpo = document.body
     corpo.classList.add('degrade3')
+    var nome = document.getElementById('nomes');
+    nome.innerHTML = 'codigo'
+    nome.style.fontSize = '100pt'
+    nome.style.margin = '-180px'
 }
 
 function nfora(){
@@ -51,4 +90,61 @@ function nfora(){
     n.style.fontSize = '20pt'
     var corpo = document.body
     corpo.classList.remove('degrade3')
+    var nome = document.getElementById('nomes')
+    nome.innerHTML = ''
+    nome.style.fontSize = '250px'
+}
+
+function sk(){
+    skills.style.color = 'black'
+    skills.style.fontSize = '30pt'
+    var corpo = document.body
+    corpo.classList.add('degrade4')
+    var nome = document.getElementById('nomes');
+    nome.innerHTML = 'skills'
+}
+
+function skfora(){
+    skills.style.color = 'white'
+    skills.style.fontSize = '20pt'
+    var corpo = document.body
+    corpo.classList.remove('degrade4')
+    var nome = document.getElementById('nomes')
+    nome.innerHTML = ''
+}
+
+function ex(){
+    extras.style.color = 'black'
+    extras.style.fontSize = '30pt'
+    var corpo = document.body
+    corpo.classList.add('degrade5')
+    var nome = document.getElementById('nomes');
+    nome.innerHTML = 'extras'
+}
+
+function exfora(){
+    extras.style.color = 'white'
+    extras.style.fontSize = '20pt'
+    var corpo = document.body
+    corpo.classList.remove('degrade5')
+    var nome = document.getElementById('nomes')
+    nome.innerHTML = ''
+}
+
+function co(){
+    contato.style.color = 'black'
+    contato.style.fontSize = '30pt'
+    var corpo = document.body
+    corpo.classList.add('degrade6')
+    var nome = document.getElementById('nomes');
+    nome.innerHTML = 'contato'
+}
+
+function cofora(){
+    contato.style.color = 'white'
+    contato.style.fontSize = '20pt'
+    var corpo = document.body
+    corpo.classList.remove('degrade6')
+    var nome = document.getElementById('nomes')
+    nome.innerHTML = ''
 }
