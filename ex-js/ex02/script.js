4f363573a944eaadeaf0ff0a1b3e58814b11d6ef
gustavomatiass <gustavomatiass123@gmail.com>
2023-01-30 10:14:47 -0300

    melhor jogando

--- ex-js/ex02/script.js
+++ ex-js/ex02/script.js
@@ -1,14 +1,14 @@
-var me = document.getElementById('messi');
-me.addEventListener('mouseenter', mover);
-me.addEventListener('mouseenter', emover);
+var n = document.getElementById('nome');
+var ronaldo = document.getElementById('cr7');
+var leonel = document.getElementById('messi');
 
-function mover(){
-    setTimeout(function() {
-        img.style.position = "absolute";
-        img.style.top = "50px";
-        img.style.left = "100px";
-      }, 2000);
+leonel.addEventListener('mouseenter', mudar);
+leonel.addEventListener('mouseout', messi);
+
+function mudar(){
+  var imgelement = document.getElementById('myimg').src = 'img/cr7.jpeg'
 }
 
-
-
+function messi(){
+  var imgelement = document.getElementById('myimg').src = 'img/messi.jpeg'
+}
