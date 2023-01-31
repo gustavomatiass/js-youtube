7f117b14bc42964cb3e993e5aa7f7c59fb6c42d2
gustavomatiass <gustavomatiass123@gmail.com>
2023-01-31 09:05:01 -0300

    erro

--- ex-js/ex03/script.js
+++ ex-js/ex03/script.js
@@ -2,6 +2,44 @@
 var btn = document.getElementById('b');
 btn.addEventListener('click', enviar);
 
+
+function enviar() {
+  var nome = document.getElementById('nomeCliente').value;
+  var idade = document.getElementById('numeroIdade').value;
+  var emailCliente = document.getElementById('email').value;
+  var senharCliente = document.getElementById('senhar').value;
+  var BT = document.getElementById('b02');  
+
+}
+
+console.log(nome + idade + emailCliente + senharCliente)
+
+
+/*
+var nome = document.getElementById('nomeCliente').value;
+var idade = document.getElementById('numeroIdade').value;
+var emailCliente = document.getElementById('email').value;
+var senharCliente = document.getElementById('senhar').value;
+var BT = document.getElementById('b02');
+
+BT.addEventListener('click', mostrar)
+
+function mostrar(){
+  var mostrar = document.getElementById('sn');
+  
+  mostrar.innerHTML = 'nome'
+  
+}
+
+
+
+
+
+
+var btn = document.getElementById('b');
+btn.addEventListener('click', enviar);
+
+
 function enviar() {
   var nome = document.getElementById('nomeCliente').value;
   var idade = document.getElementById('numeroIdade').value;
@@ -12,3 +50,4 @@ function enviar() {
   var mostrarnome = document.getElementById('n')
   mostrarnome.innerHTML = nome
 }
+*/
